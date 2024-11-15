1dc41be7f60c8d52cd509964ddf6c2f535bd8a22
NovySoyuz <florentcabassut@orange.fr>
2024-11-15 10:38:27 +0100

    git ignore

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 // Importation de la librairie
 const mysql = require('mysql2');
+// Charge les variables d'environnement
+require('dotenv').config();
 // Instantation de l'objet
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'cube4group'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
