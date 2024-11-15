23b32effea56c74b8aad193c8ebc06ac4326fe22
Sabrina <sabrina.talbi@viacesi.fr>
2024-11-15 10:29:37 +0100

    dotenv

--- config/db.js
+++ config/db.js
@@ -1,19 +1,17 @@
-const mysql = require('mysql');
+// Connexion à la base de donnée
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'fats4',
-    database: 'cubes4group'
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
-
-db.connect((err) => {
-    if (err) {
-        console.error('Erreur de connexion à la base de données:', err);
-        process.exit(1);
-    }
-    console.log('Connecté à la base de données');
-});
-
-module.exports = db;
+module.exports = pool.promise();
