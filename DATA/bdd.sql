1d3a72918f091b02b47a57e420deeefb93e3ddaf
Florent <150264637+NovySoyuz@users.noreply.github.com>
2024-12-03 11:39:42 +0100

    Update bdd.sql / Gender VARCHAR(6) to VARCHAR(8)

--- DATA/bdd.sql
+++ DATA/bdd.sql
@@ -7,7 +7,7 @@ CREATE TABLE Users(
                       password VARCHAR(255) ,
                       first_name VARCHAR(255) ,
                       birthday DATE,
-                      gender VARCHAR(6) ,
+                      gender VARCHAR(8) ,
                       last_name VARCHAR(255) ,
                       isAdmin BOOLEAN,
                       PRIMARY KEY(Id_Users)
