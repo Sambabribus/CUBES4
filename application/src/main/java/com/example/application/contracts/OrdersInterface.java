ad710f4ab5ddff93188f35b82b4c1366cc6bc143
NovySoyuz <florentcabassut@orange.fr>
2025-01-31 08:45:00 +0100

    Version final

--- application/src/main/java/com/example/application/contracts/OrdersInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.application.contracts;
-
-import com.example.application.model.Orders;
-
-import java.util.List;
-
-public interface OrdersInterface {
-    List<Orders> getAllOrders();
-}
